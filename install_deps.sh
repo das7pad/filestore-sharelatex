19f4a57e59edc8d51c5698a8924882f0b67af78a
Jakob Ackermann <das7pad@outlook.com>
2020-08-17 01:13:11 +0200

    [misc] fix image conversion

--- install_deps.sh
+++ install_deps.sh
@@ -7,3 +7,7 @@ apt-get update
 apt-get install ghostscript imagemagick optipng --yes
 
 rm -rf /var/lib/apt/lists/*
+
+# https://stackoverflow.com/questions/52998331/imagemagick-security-policy-pdf-blocking-conversion#comment110879511_59193253
+# License: CC BY-SA 4.0
+sed -i '/disable ghostscript format types/,+6d' /etc/ImageMagick-6/policy.xml
